4d493118cd5b45ebd8ab369d3b513af764470fce
matti09 <cocciamatti@hotmail.com>
2025-05-17 09:10:10 +0200

    Create test.ts

---
+++ test.ts
@@ -0,0 +1,9 @@
+MFRC522.Init() → Initialisiert das RFID-Modul.
+
+MFRC522.getID() → Liest die UID der Karte.
+
+MFRC522.read() → Ruft gespeicherte Daten aus der Karte ab.
+
+MFRC522.write("1234") → Schreibt „1234“ auf die Karte.
+
+MFRC522.AntennaOff() → Schaltet die Antenne aus.
